56196a58afb64b63cc045c77999f8dea96ea4b7b
jiwoo <bungaedm@naver.com>
2024-02-05 10:39:25 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+
+function Counter(){
+  return <div>
+    <h1>Counter</h1>
+    <button>+</button> 0
+  </div>
+}
 
 function App() {
   return (
     <div>
-      hi
+      <Counter />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
