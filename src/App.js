2d89070660fc5daa0f53bad1a5d85ab0d4977635
jiwoo <bungaedm@naver.com>
2024-02-05 17:35:46 +0900

    CSS module update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import style from './App.module.css';
 
 function Counter({title, initValue}){
   const [value, setValue] = useState(initValue);
@@ -35,13 +36,15 @@ function Counter({title, initValue}){
     setHistory(newHistory);
   };
 
+  const h1Text = {color:'red', fontSize:'30px'}
+
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className={style.layout}>
+      <h1 style={h1Text}>{title}</h1>
       <input type="number" value={step} onChange={(evt)=>{
         setStep(Number(evt.target.value));
       }}/>
-      <button onClick={up}>+</button> {value}
+      <button onClick={up} className='btn'>+</button> {value}
       <ol>
         {history.map((item,index)=>{
           return <li key={index}>{item}</li>
