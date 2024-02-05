0dcf4f26ce9a7df3ae8c81922abd937805e92411
jiwoo <bungaedm@naver.com>
2024-02-05 10:49:12 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 
-function Counter(){
+function Counter(props){
+  console.log(props.title);
   return <div>
-    <h1>Counter</h1>
-    <button>+</button> 0
+    <h1>{props.title}</h1>
+    <button>+</button> 5
   </div>
 }
 
 function App() {
   return (
     <div>
-      <Counter />
+      <Counter title="카운터"/>
+      <Counter title="불면증 카운터"/>
     </div>
   );
 }
