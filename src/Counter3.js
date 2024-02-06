1a516014236763c7f3477f4a84f024ba1229e5d0
jiwoo <bungaedm@naver.com>
2024-02-06 10:07:03 +0900

    async await application

--- src/Counter3.js
+++ src/Counter3.js
@@ -4,24 +4,32 @@ function Counter3({title, initValue}){
     const [value, setValue] = useState(initValue);
     const [step, setStep] = useState(10);
     
-    // useEffect(()=>{
-    //     fetch('http://localhost:9999/counter')
-    //     .then(res=>res.json())
-    //     .then(data=>{
-    //         setValue(data.value);
-    //     })
-    // },[])
-
     useEffect(()=>{
-        (async () => {
-            const res = await fetch('http://localhost:9999/counter')
-            const data = await res.json();    
+        fetch('http://localhost:9999/counter')
+        .then(res=>res.json())
+        .then(data=>{
             setValue(data.value);
-        })();
+        })
     },[])
 
-    function up(){
+    // useEffect(()=>{
+    //     (async () => {
+    //         const res = await fetch('http://localhost:9999/counter')
+    //         const data = await res.json();    
+    //         setValue(data.value);
+    //     })();
+    // },[])
+
+    async function up(){
         setValue(value+step);
+        const resp = await fetch('http://localhost:9999/counter',{
+            method:'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({value:value+1})
+        });
+        await resp.json();
     }
     function stepChange(e){
         setStep(Number(e.target.value));
